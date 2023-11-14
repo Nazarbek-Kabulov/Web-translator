ebe2c61cf25fdd8e5ccf0862f5b7b42e6ac0a769
Kabulov-Nazarbek <nazarbekqobulov28@gmail.com>
2023-11-14 22:20:37 +0500

    ..

--- web_translate/settings.py
+++ web_translate/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l+swq7jf^3=71x+*mfs%e@+y^+8xx3zo(!+e5!#4#m0=_mrr%m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.pythonanywhere.com', '127.0.0.1']
 
